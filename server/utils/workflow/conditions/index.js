a285eb503a885788eea1d475a168593c6d61302b
nahid071 <mdnahid071@gmail.com>
2024-09-01 13:43:31 +0600

    condition centralize

--- server/utils/workflow/conditions/index.js
+++ server/utils/workflow/conditions/index.js
@@ -1,23 +1,35 @@
 // Checker Imports
-const hasVisitedPage = require('../conditions/hasVisitedPage');
+const hasVisitedPage = require("../conditions/hasVisitedPage");
+// const hasSignedUp = require('../conditions/hasSignedUp');
+// const hasPurchasedCourse = require('../conditions/hasPurchasedCourse');
 
+// Condition Map
+const conditionFunctions = {
+  hasVisitedPage,
+  // hasSignedUp,
+  // hasPurchasedCourse,
+};
 
-// Checking Function
-function checkValidity(activityType, data){
-    switch(activityType){
-        case "hasvisitedpage": 
-            if(hasVisitedPage(data?.user, data?.pageName)){
-                return true;
-            }
-            return false
-        // add other cases
-        default:
-            return false;
+function checkValidity(data) {
+  const { conditions, logic, checkerInfo } = data;
+
+  const conditionResults = conditions.map((condition) => {
+    const checkFunction = conditionFunctions[condition];
+    if (checkFunction) {
+      return checkFunction(checkerInfo);
     }
+
+    return false;
+  });
+
+  // Combine results based on the logic (AND/OR)
+  if (logic === "AND") {
+    return conditionResults.every((result) => result === true);
+  } else if (logic === "OR") {
+    return conditionResults.some((result) => result === true);
+  }
+
+  return false;
 }
 
-
-
-module.exports = {
-    checkValidity
-}
+module.exports = checkValidity;
--- server/utils/workflow/triggers/handlePageVisit.js
+++ server/utils/workflow/triggers/handlePageVisit.js
@@ -1,20 +1,26 @@
-const hasVisitedPage = require("../conditions/hasVisitedPage");
 const StartWorkflow = require("../workflowLauncher");
 const getWorkflow = require("../api/workflowData");
-const {checkValidity} = require('../conditions/index');
+const conditionValidator = require("../conditions/index");
 
 // "page_visited" is the trigger name. this name is unique for each trigger function.
 // in this case trigger name for handlePageVisit function is "page_visited"
 // pass the trigger name to getWorkflow function inside every trigger function
+// Use the conditionValidator() function to check, if the conditions are fulfilled or not
 
-// Use the checkValidity() function to check, if the condition is fulfilled or not
+const validationData = {
+  conditions: ["hasVisitedPage", "hasSignedUp", "hasPurchasedCourse"],
+  logic: "AND",
+  checkerInfo: { userId: "123", pageName: "calendar" },
+};
 
 function handlePageVisit(user, pageName) {
-  if (checkValidity("hasvisitedpage", {user, pageName})) {
-    getWorkflow("page_visited").then((result) => {
-      if (result.exists) StartWorkflow(user, pageName, result.workflow);
-    });
-  }
+  getWorkflow("page_visited").then((result) => {
+    if (result.exists) {
+      if (conditionValidator(validationData)) {
+        StartWorkflow(user, pageName, result.workflow);
+      }
+    }
+  });
 }
 
 module.exports = handlePageVisit;
